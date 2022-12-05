24df598fd1e24ef621938b264b317784cfef546d
seppotk <seppo.kallio2@metropolia.fi>
2022-12-05 21:59:12 +0200

    cors

--- server.js
+++ server.js
@@ -6,16 +6,24 @@ import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
+
+
+
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
+import cors from 'cors'
+
+
 
 dotenv.config()
 
 connectDB()
 
 const app = express()
+
+app.use(cors())
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
