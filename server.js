f31b5a94b26566419e3f6a996cedbafeb684d162
seppotk <seppo.kallio2@metropolia.fi>
2022-12-05 22:01:38 +0200

    cors 	modified:   server.js

--- server.js
+++ server.js
@@ -23,12 +23,13 @@ connectDB()
 
 const app = express()
 
-app.use(cors())
+
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/products', productRoutes)
